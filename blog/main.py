9c60998634798d76a2096a6dad78037783a5b5fc
osantau <octavian.santau.os@gmail.com>
2025-07-13 10:34:38 +0300

    Implement user creation and getting

--- blog/main.py
+++ blog/main.py
@@ -106,10 +106,17 @@ def udpate_blog(id:int,request: schemas.Blog, db: Session = Depends(get_db)):
 # if __name__ == "__main__":
 #     uvicorn.run(app, host="127.0.0.1", port=9000)
 
-@app.post("/user")
+@app.post("/user", response_model=schemas.ShowUser)
 def create_user(request: schemas.User,db: Session = Depends(get_db)):   
     new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@app.get("/user/{id}",status_code=status.HTTP_200_OK, response_model=schemas.ShowUser)
+def get_user(id:int,db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with {id} was not found!")
+    return user
