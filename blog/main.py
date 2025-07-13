ea1cfa5e61ef42b8aa9f48e3a20b6f389a0ed24a
osantau <octavian.santau.os@gmail.com>
2025-07-13 09:45:46 +0300

    Change git ignore. Finish Update action.

--- blog/main.py
+++ blog/main.py
@@ -83,13 +83,21 @@ def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
 
 @app.delete("/blog/{id}",status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id:int, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"The blog with id {id} not exists !")
+    
+    blog.delete(synchronize_session=False)
     db.commit()
     return "done"    
 
 @app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
 def udpate_blog(id:int,request: schemas.Blog, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id==id).update(request,synchronize_session=False) 
+    blog = db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"The blog with id {id} not exists !")
+    
+    blog.update({'title':request.title,'body':request.body}) 
     db.commit()
     return "updated successfully"     
     
