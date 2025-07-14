455beb99dd3aa66fb96b67173e99e4c9b498b81c
osantau <octavian.santau@plastor.ro>
2025-07-14 12:59:12 +0300

    fetch blog with user and user with all blogs.

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,14 +1,24 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 
 class Blog(BaseModel):
     title: str
     body: str
-    published: Optional[bool]
+    # published: Optional[bool]
+    class Config():
+        from_attributes = True
 
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    blogs: List[Blog]=[]
+    class Config():
+        from_attributes = True
+        
 class ShowBlog(BaseModel):
     title: str
     body: str
+    creator: ShowUser
     class Config():
         from_attributes = True
 
@@ -16,10 +26,5 @@ class User(BaseModel):
     name: str
     email: str
     password: str
-
-class ShowUser(BaseModel):
-    name: str
-    email: str
     class Config():
         from_attributes = True
-        
